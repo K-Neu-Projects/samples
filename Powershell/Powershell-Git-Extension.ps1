5a4263e3b2702f03ad9945af903355e8046a74f2
kneu <k.neu@slashwhy.de>
2021-10-01 00:14:41 +0200

    add posh-git

--- Powershell/Powershell-Git-Extension.ps1
+++ Powershell/Powershell-Git-Extension.ps1
@@ -5,8 +5,10 @@
 
 # Install git extension for powershell
 Install-Module PowerShellForGitHub -Scope CurrentUser
+Install-Module posh-git
 
 # Add to $profile
 # Note that there are multiple profiles e.g. executing this within VS Code will only add it to 
 # the vsCode_profile, not the global one. PowerShell ISE also has its own profile
 Write-Output "Import-Module PowerShellForGitHub" >> $profile
+Write-Output "Import-Module posh-git" >> $profile
