4930e909d969a6e4cb918cbb5506856cef7c6b47
MirrorsOnTheCeiling <kneu.projects@gmail.com>
2023-01-18 13:49:23 +0100

    use key file directly in jwt generation script

--- Python/jwt/generate_jwt.py
+++ Python/jwt/generate_jwt.py
@@ -1,8 +1,9 @@
 import jwt
 import time
+import os
 
 # Set the secret key
-secret_key = ""
+secret_key = open("private_key.pem", "r").read()
 
 # Set the key id to use
 kid = ""
@@ -10,16 +11,20 @@ kid = ""
 # Get the current time
 iat = int(time.time())
 
+header = {
+    "kid": kid,
+    "alg": "RS256"
+}
+
 # Set the payload (data to be encoded)
 payload = {
-    "kid": kid,
     "iss": "dev_python_script",
     "iat": iat,
     "exp": iat + 60*60*24*7*52
 }
 
 # Encode the payload with the secret key and return the JWT
-jwt_token = jwt.encode(payload, secret_key, algorithm='HS256')
+jwt_token = jwt.encode(payload, secret_key, algorithm='RS256', headers=header)
 
 # Print the JWT
 print(jwt_token)
